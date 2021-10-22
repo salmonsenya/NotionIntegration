8ae628f044474fac7344811adca25f12a947d04d
Kseniia Khromova <kseniiakhromova@yandex.ru>
2021-10-22 23:33:47 +0300

    Web app initial template created

---
+++ NotionIntegration/Program.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace NotionIntegration
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>()
+                    .UseKestrel(options => options.Listen(IPAddress.Loopback, 3000));
+                });
+    }
+}
